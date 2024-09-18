cedaee5b2b6af6fc5f7fbd1100daf7de016b0d04
AtifSyed2nd <123717793+AtifSyed2nd@users.noreply.github.com>
2024-09-18 18:10:47 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -98,15 +98,22 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.mysql',
+# 		'NAME': 'greentech',
+# 		'USER': 'root',
+# 		'PASSWORD': 'root',
+# 		'HOST':'localhost',
+# 		'PORT':'3306',
+# 	}
+# }
+
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.mysql',
-		'NAME': 'greentech',
-		'USER': 'root',
-		'PASSWORD': 'root',
-		'HOST':'localhost',
-		'PORT':'3306',
-	}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
